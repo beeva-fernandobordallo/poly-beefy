41fb59633f66417db5a48e51f9a05721bbdc7f87
FBV <fbordallo2@gmail.com>
2016-08-21 16:12:29 +0200

    [structure] - clear out token dev routes

---
+++ project/server-dev/app/api/devControllers/general.controller.js
@@ -0,0 +1,21 @@
+'use strict';
+const jwt = require('jsonwebtoken');
+const config = require('../../../config/server');
+
+exports.returnWithToken = (req, res) => {
+	if(req.user){
+		const encryptData = {
+			id: req.user._id,
+			googleId: req.user.google.id,
+			displayName: req.user.google.displayName,
+			role: req.user.role
+		};
+		const token = jwt.sign(encryptData, config.serverSecret, { expiresIn: 120 }); // in seconds (2 minutes)
+		const responseObj = {
+			token
+		};
+		res.end(JSON.stringify(responseObj));
+	} else {
+		res.status(500).end(JSON.stringify({msg: 'No dev user created. Server error'}));
+	}
+};
--- project/server-dev/app/api/devRoutes.js
+++ project/server-dev/app/api/devRoutes.js
@@ -2,8 +2,8 @@
 
 // Import used modules
 const express = require('express');
-const jwt = require('jsonwebtoken');
 const userDevCtrl = require('./devControllers/user.controller');
+const generalDevCtrl = require('./devControllers/general.controller')
 
 // Main route setup function exported
 module.exports = function devApiRoutesetup(app) {
@@ -11,88 +11,17 @@ module.exports = function devApiRoutesetup(app) {
 	const devApiRoutes = express.Router();
 
 	/**
-	 * Request USER ROLE TOKEN
+	 * Request TOKENs
 	 */
-	devApiRoutes.get('/user-token', userDevCtrl.devUser, (req, res) => {
-		if(req.user){
-			const encryptData = {
-				id: req.user._id,
-				googleId: req.user.google.id,
-				displayName: req.user.google.displayName,
-				role: req.user.role
-			};
-			const token = jwt.sign(encryptData, app.get('secret'), { expiresIn: 120 }); // in seconds (2 minutes)
-			const responseObj = {
-				token
-			};
-			res.end(JSON.stringify(responseObj));
-		} else {
-			res.status(500).end(JSON.stringify({msg: 'No dev user created. Server error'}));
-		}
-	});
+	// User
+	devApiRoutes.get('/user-token', userDevCtrl.devUser, generalDevCtrl.returnWithToken);
+	// Admin
+	devApiRoutes.get('/admin-token', userDevCtrl.adminUser, generalDevCtrl.returnWithToken);
+	// Financial
+	devApiRoutes.get('/financial-token', userDevCtrl.financeUser, generalDevCtrl.returnWithToken);
+	// Web Master
+	devApiRoutes.get('/master-token', userDevCtrl.webmasterUser, generalDevCtrl.returnWithToken);
 
-	/**
-	 * Request USER ROLE TOKEN
-	 */
-	devApiRoutes.get('/admin-token', userDevCtrl.adminUser, (req, res) => {
-		if(req.user){
-			const encryptData = {
-				id: req.user._id,
-				googleId: req.user.google.id,
-				displayName: req.user.google.displayName,
-				role: req.user.role
-			};
-			const token = jwt.sign(encryptData, app.get('secret'), { expiresIn: 120 }); // in seconds (2 minutes)
-			const responseObj = {
-				token
-			};
-			res.end(JSON.stringify(responseObj));
-		} else {
-			res.status(500).end(JSON.stringify({msg: 'No dev user created. Server error'}));
-		}
-	});
-
-	/**
-	 * Request USER ROLE TOKEN
-	 */
-	devApiRoutes.get('/financial-token', userDevCtrl.financeUser, (req, res) => {
-		if(req.user){
-			const encryptData = {
-				id: req.user._id,
-				googleId: req.user.google.id,
-				displayName: req.user.google.displayName,
-				role: req.user.role
-			};
-			const token = jwt.sign(encryptData, app.get('secret'), { expiresIn: 120 }); // in seconds (2 minutes)
-			const responseObj = {
-				token
-			};
-			res.end(JSON.stringify(responseObj));
-		} else {
-			res.status(500).end(JSON.stringify({msg: 'No dev user created. Server error'}));
-		}
-	});
-
-	/**
-	 * Request USER ROLE TOKEN
-	 */
-	devApiRoutes.get('/master-token', userDevCtrl.webmasterUser, (req, res) => {
-		if(req.user){
-			const encryptData = {
-				id: req.user._id,
-				googleId: req.user.google.id,
-				displayName: req.user.google.displayName,
-				role: req.user.role
-			};
-			const token = jwt.sign(encryptData, app.get('secret'), { expiresIn: 120 }); // in seconds (2 minutes)
-			const responseObj = {
-				token
-			};
-			res.end(JSON.stringify(responseObj));
-		} else {
-			res.status(500).end(JSON.stringify({msg: 'No dev user created. Server error'}));
-		}
-	});
 
 	app.use('/dev-api', devApiRoutes);
 
