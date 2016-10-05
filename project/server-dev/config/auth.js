df85bcdaddd30e3917dcc367d6bcacc683decf47
FBV <fbordallo2@gmail.com>
2016-10-05 09:08:31 +0200

    [deployFix] include deployed machine URL callback

--- project/server-dev/config/auth.js
+++ project/server-dev/config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
 	googleAuth: {
 		clientID: '288179133923-bb6s17tl9f3f60m77ma4mbmve1fuu7f0.apps.googleusercontent.com',
 		clientSecret: '5QVuOlVLjUFpd2jNoYfhXFUv',
-		callbackURL: 'http://localhost:3000/api/auth/google/callback'
+		callbackURL: (process.env.NODE_ENV === 'dev') ? 'http://localhost:3000/api/auth/google/callback' : 'http://beeva-externa.westeurope.cloudapp.azure.com:8080/api/auth/google/callback'
 	}
 
 };
